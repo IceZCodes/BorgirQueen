a99b55ef5e73e8e7c4fc5f02f1ba776ac5838219
alfianzv <alfian101@gmail.com>
2022-11-03 20:21:37 +0700

    feat: added superborgir card

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -1,7 +1,20 @@
 @extends('master.master')
-
+<link href="{{ asset('css/index.css') }}" rel="stylesheet">
 @section('content')
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illum velit, eaque iste obcaecati
-        placeat laborum quod
-        quam quia nesciunt quas illo dignissimos corrupti odio? Unde modi qui tenetur provident? </p>
+    <div class="relative landing h-screen text-[#2B3437] opacity-100">
+        <div class="tagline absolute bg-[#FFFFFFE5]  rounded-tl-[4rem] rounded-br-[4rem] p-10">
+            <div class="font-bold text-5xl">
+                Super Borgir Queen™
+            </div>
+            <div class="mt-4 font-light text-xl">
+                The best borgir for all
+            </div>
+            <button>
+                
+            </button>
+        </div>
+        <div class="right">
+
+        </div>
+    </div>
 @endsection
