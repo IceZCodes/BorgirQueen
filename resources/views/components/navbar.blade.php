922c9a9ce91f9db35c29362a9400706eb2de66ca
alfianzv <alfian101@gmail.com>
2022-11-03 19:22:15 +0700

    feat: added navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<div class="fixed h-16 w-full flex flex-row justify-between items-center bg-[#FEFDFB] drop-shadow-md px-36">
+    <div class="text-[#EE3E42]">
+        Borgir<span class="text-[#F9AA53]">Queen</span>
+    </div>
+    <ul class="flex flex-row font-semibold text-[#2B3437] gap-x-3">
+        <li class="text-[#007AC2]">Home</li>
+        <a href="#" class="hover:text-[#007AC2]">Menu</a>
+        <a href="#" class="hover:text-[#007AC2]">Deals</a>
+        <a href="#" class="hover:text-[#007AC2]">Cart</a>
+        <a href="#" class="hover:text-[#007AC2]">Order Now</a>
+    </ul>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('page.welcome');
 });
+
+Route::get('/index', function () {
+    return view('page.index');
+});
