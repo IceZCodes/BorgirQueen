5d43835db775154cbd52c20ddc480f9460309b9a
alfianzv <alfian101@gmail.com>
2023-01-19 14:32:28 +0700

    feat: added favicon

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{asset ('assets\BorgirQueen.png')}}">
     <title>{{ $title }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
