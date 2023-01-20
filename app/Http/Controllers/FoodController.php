612642b6fad629cb3ec82c38564f66ba46f531bd
ben@uc98.com <ben@uc98.com>
2023-01-20 21:25:07 +0700

    remove error add to cart

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -60,10 +60,6 @@ class FoodController extends Controller
         $itemsPrice = Food::findOrFail($id)->price * $req->foodQty;
         $checkIfExist = FoodCart::where('cart_id', $cart->id)->where('food_id', $id)->first();
 
-        $req->validate([
-            'food_Qty' => ['required', 'integer'],
-        ]);
-
         if ($checkIfExist) {
             $checkIfExist->update([
                 'qty' => $checkIfExist->qty + $req->foodQty,
