4de590cc7a88eed60fad8a49f3fa1d909a5be505
Bagus Saputra <audubon000@gmail.com>
2023-01-24 18:54:36 +0700

    fix remember me

--- resources/views/page/login/index.blade.php
+++ resources/views/page/login/index.blade.php
@@ -15,22 +15,27 @@
 
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
-                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 @error('email') is-invalid @enderror" placeholder="armaniferrante@gmail.com" required autocomplete="email" autofocus>
+                        <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="armaniferrante@gmail.com" required autocomplete="email" autofocus>
 
                         @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                {{ $message }}
+                            <span class="text-red-700 invalid-feedback" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
                             </span>
                         @enderror
                     </div>
 
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required>
+
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="form-control @error('password') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required>
 
                         @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                {{ $message }}
+                            <span class="text-red-700 invalid-feedback" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
                             </span>
                         @enderror
                     </div>
@@ -38,13 +43,14 @@
                     <div class="flex items-center justify-between">
                         <div class="flex items-start">
                             <div class="form-check flex items-center h-5">
-                              <input id="remember" aria-describedby="remember" type="checkbox" class="form-check-input w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" {{ old('remember') ? 'checked' : '' }}>
+                              <input id="remember"
+                              name="remember"
+                              aria-describedby="remember" type="checkbox" class="form-check-input w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" value="1" {{ old('remember') ? 'checked' : '' }}>
                             </div>
                             <div class="form-check-label ml-3 text-sm">
                               <label for="remember" class="text-gray-500">Remember me</label>
                             </div>
                         </div>
-                        <a href="#" class="text-sm font-medium text-blue-500 hover:underline">Forgot password?</a>
                     </div>
                     
                     <button type="submit" class="w-full text-white bg-[#005BAA] hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
